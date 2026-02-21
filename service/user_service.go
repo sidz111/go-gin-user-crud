1e957e04ed34e5b7f657fc11b5beffc786a1557b
sidz111 <sssurwade2212@gmail.com>
2026-02-21 22:40:59 +0530

    Worked on Service Layer

--- service/user_service.go
+++ service/user_service.go
@@ -25,6 +25,47 @@ func (s *UserService) Create(user *model.User) (*model.User, error) {
 	return user, nil
 }
 
+func (s *UserService) GetByID(id int) (*model.User, error) {
+	if id < 1 {
+		return nil, fmt.Errorf("Id is Invalid")
+	}
+	user, err := s.repo.GetByid(id)
+	if err != nil {
+		return nil, err
+	}
+	return user, nil
+}
+
+func (s *UserService) DeletebyId(id int) error {
+	if id < 1 {
+		return fmt.Errorf("Id is Invalid")
+	}
+	return s.DeletebyId(id)
+}
+
+func (s *UserService) GetAllUsers() ([]model.User, error) {
+	return s.repo.GetAllUsers()
+}
+
+func (s *UserService) UpdateUser(id int, user *model.User) error {
+	if id < 1 {
+		return fmt.Errorf("Id is Invalid")
+	}
+
+	u, err := s.repo.GetByid(id)
+	if err != nil {
+		return fmt.Errorf("User Not Found")
+	}
+
+	if user.Name != "" {
+		u.Name = user.Name
+	}
+	if user.Address != "" {
+		u.Address = user.Address
+	}
+	return s.repo.UpdateUser(id, u)
+}
+
 func ValidateUser(user *model.User) error {
 	if user.Name == "" {
 		return fmt.Errorf("Name is Required")
